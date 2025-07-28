8333317a03ac67631a037a270f3220210ddd1b2a
Lucas Mendonca <lucasmendev@proton.me>
2025-07-28 09:30:00 -0400

    commit sprintboot project

---
+++ src/test/java/com/project/java10x/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.java10x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
