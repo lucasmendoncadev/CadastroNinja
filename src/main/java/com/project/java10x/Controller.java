8f1726b5c508f2e47000b2daf57a13eec025e2b9
Lucas Mendonca <lucasmendev@proton.me>
2025-07-29 11:12:12 -0400

    added Controller

---
+++ src/main/java/com/project/java10x/Controller.java
@@ -0,0 +1,18 @@
+package com.project.java10x;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to my API";
+    }
+
+
+
+}
